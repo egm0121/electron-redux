fc640be9cb6e3b8cad9c5f4e931b1732960257a9
Giulio Dellorbo <giulio.dellorbo@dxi.eu>
2018-03-28 18:12:41 +0100

    wip fork of electron-redux to preseve sync dispatching of actions within the originating process

--- packages/electron-redux/src/helpers/replayActionRenderer.js
+++ packages/electron-redux/src/helpers/replayActionRenderer.js
@@ -1,7 +1,13 @@
 import { ipcRenderer } from 'electron';
 
-export default function replayActionRenderer(store) {
-  ipcRenderer.on('redux-action', (event, payload) => {
-    store.dispatch(payload);
-  });
+export default function replayActionRenderer(processName) {
+  return function(store) {
+    ipcRenderer.on('redux-action', (event, payload) => {
+      if (!payload.meta || payload.meta.originProcessId !== processName) {
+        store.dispatch(payload);
+      } else {
+        console.log('local action detected do not redispatch', payload);
+      }
+    });
+  };
 }
--- packages/electron-redux/src/middleware/forwardToMain.js
+++ packages/electron-redux/src/middleware/forwardToMain.js
@@ -1,23 +1,26 @@
 import { ipcRenderer } from 'electron';
 import validateAction from '../helpers/validateAction';
 
-const forwardToMain = store => next => (action) => { // eslint-disable-line no-unused-vars
+const forwardToMain = processName => store => next => action => {
+  // eslint-disable-line no-unused-vars
   if (!validateAction(action)) return next(action);
 
   if (
-    action.type.substr(0, 2) !== '@@'
-    && action.type.substr(0, 10) !== 'redux-form'
-    && (
-      !action.meta
-      || !action.meta.scope
-      || action.meta.scope !== 'local'
-    )
+    action.type.substr(0, 2) !== '@@' &&
+    action.type.substr(0, 10) !== 'redux-form' &&
+    (!action.meta || !action.meta.scope || action.meta.scope !== 'local')
   ) {
-    ipcRenderer.send('redux-action', action);
-
+    const prevMeta = action.meta || {};
+    const remoteAction = {
+      ...action,
+      meta: {
+        ...prevMeta,
+        originProcessId: processName,
+      },
+    };
+    ipcRenderer.send('redux-action', remoteAction);
     // stop action in-flight
     // eslint-disable-next-line consistent-return
-    return;
   }
 
   // eslint-disable-next-line consistent-return
